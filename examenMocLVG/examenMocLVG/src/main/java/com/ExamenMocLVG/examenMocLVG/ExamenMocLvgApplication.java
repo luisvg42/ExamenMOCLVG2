51c817b97e5d1c5edd4f121bed95935d30443160
luisvg42 <luisvg42@educastur.es>
2025-11-21 17:57:37 +0100

    first commit

---
+++ examenMocLVG/examenMocLVG/src/main/java/com/ExamenMocLVG/examenMocLVG/ExamenMocLvgApplication.java
@@ -0,0 +1,13 @@
+package com.ExamenMocLVG.examenMocLVG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocLvgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocLvgApplication.class, args);
+	}
+
+}
---
+++ examenMocLVG/examenMocLVG/src/test/java/com/ExamenMocLVG/examenMocLVG/ExamenMocLvgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExamenMocLVG.examenMocLVG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocLvgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
